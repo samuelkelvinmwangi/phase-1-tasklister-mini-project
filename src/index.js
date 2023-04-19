5bac506d788614deb7083ed7410807a191c09e29
samuel <samuelkelvinmwangi@gmail.com>
2023-04-19 12:17:45 +0300

    assingnment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e,preventDefault()
+    newTask(e.target.new-task-description.value)
+    form.reset()
+  })
+})
+
+function newTask(task){
+  let label = document.createElement('label')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+
+    btn.textContent = 'x'
+
+    label.textContent = ` ${task} `
+    label.appendChild(btn)
+
+    console.log(label)
+
+    document.querySelector('label[for="new-task-description"]').appendChild(label)
+}
+
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
